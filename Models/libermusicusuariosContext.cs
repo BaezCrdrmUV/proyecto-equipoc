18d0e0eca5264dfc5e755a0f6554a81d750f00cb
aaron-77 <aaronhl1313@gmail.com>
2021-05-21 16:23:33 -0500

    actualizando a los cambios mas recientes para simulacion de prueba de servicios

--- Models/libermusicusuariosContext.cs
+++ Models/libermusicusuariosContext.cs
@@ -6,25 +6,28 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace MSSuscripcion.Models
 {
-    public partial class libermusicusuariosContext : DbContext
+    public partial class libermusicUsuariosContext : DbContext
     {
-        public libermusicusuariosContext()
+        public libermusicUsuariosContext()
         {
         }
 
-        public libermusicusuariosContext(DbContextOptions<libermusicusuariosContext> options)
+        public libermusicUsuariosContext(DbContextOptions<libermusicUsuariosContext> options)
             : base(options)
         {
         }
 
+        public virtual DbSet<Contrasena> Contrasenas { get; set; }
+        public virtual DbSet<Datosdelocalizacion> Datosdelocalizacions { get; set; }
+        public virtual DbSet<Estatusderegistro> Estatusderegistros { get; set; }
         public virtual DbSet<Usuario> Usuarios { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                 string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-              
+                string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+             
                 optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.FromString("8.0.23-mysql"),
                     builder => {
                         builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
@@ -35,16 +38,46 @@ namespace MSSuscripcion.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Usuario>(entity =>
+            modelBuilder.Entity<Contrasena>(entity =>
             {
-                entity.ToTable("usuarios");
+                entity.HasKey(e => e.FkIdUsuario)
+                    .HasName("PRIMARY");
 
-                entity.Property(e => e.Id).HasColumnName("id");
+                entity.ToTable("contrasenas");
 
-                entity.Property(e => e.Contrasena)
+                entity.Property(e => e.FkIdUsuario)
+                    .HasColumnType("varchar(200)")
+                    .HasColumnName("fkIdUsuario")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.Contrasena1)
                     .IsRequired()
                     .HasColumnType("varchar(200)")
                     .HasColumnName("contrasena")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.HasOne(d => d.FkIdUsuarioNavigation)
+                    .WithOne(p => p.Contrasena)
+                    .HasForeignKey<Contrasena>(d => d.FkIdUsuario)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_contrasenas_usuarios1");
+            });
+
+            modelBuilder.Entity<Datosdelocalizacion>(entity =>
+            {
+                entity.HasKey(e => e.FkIdUsuario)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("datosdelocalizacion");
+
+                entity.HasIndex(e => e.Email, "email_UNIQUE")
+                    .IsUnique();
+
+                entity.Property(e => e.FkIdUsuario)
+                    .HasColumnType("varchar(200)")
+                    .HasColumnName("fkIdUsuario")
                     .HasCharSet("utf8mb4")
                     .HasCollation("utf8mb4_0900_ai_ci");
 
@@ -55,15 +88,52 @@ namespace MSSuscripcion.Models
                     .HasCharSet("utf8mb4")
                     .HasCollation("utf8mb4_0900_ai_ci");
 
-                entity.Property(e => e.Estado)
+                entity.Property(e => e.Pais)
+                    .IsRequired()
+                    .HasColumnType("varchar(200)")
+                    .HasColumnName("pais")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.HasOne(d => d.FkIdUsuarioNavigation)
+                    .WithOne(p => p.Datosdelocalizacion)
+                    .HasForeignKey<Datosdelocalizacion>(d => d.FkIdUsuario)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_datosdelocalizacion_usuarios1");
+            });
+
+            modelBuilder.Entity<Estatusderegistro>(entity =>
+            {
+                entity.ToTable("estatusderegistros");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.NombreDeEstatus)
+                    .IsRequired()
                     .HasColumnType("varchar(20)")
-                    .HasColumnName("estado")
-                    .HasDefaultValueSql("'activo'")
-                    .HasComment("estatus de guardado(activo,inactivo)")
+                    .HasColumnName("nombreDeEstatus")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.ToTable("usuarios");
+
+                entity.HasIndex(e => e.FkidEstatus, "fk_usuarios_EstatusDeRegistros1_idx");
+
+                entity.HasIndex(e => e.NombreDeUsuario, "nombreDeUsuario_UNIQUE")
+                    .IsUnique();
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("varchar(200)")
+                    .HasColumnName("id")
                     .HasCharSet("utf8mb4")
                     .HasCollation("utf8mb4_0900_ai_ci");
 
                 entity.Property(e => e.FkIdArtista).HasColumnName("fkIdArtista");
+
+                entity.Property(e => e.FkidEstatus).HasColumnName("fkidEstatus");
 
                 entity.Property(e => e.NombreDeUsuario)
                     .IsRequired()
@@ -81,13 +151,11 @@ namespace MSSuscripcion.Models
                     .HasCharSet("utf8mb4")
                     .HasCollation("utf8mb4_0900_ai_ci");
 
-                entity.Property(e => e.Pais)
-                    .IsRequired()
-                    .HasColumnType("varchar(200)")
-                    .HasColumnName("pais")
-                    .HasComment("pais de origen del usuario")
-                    .HasCharSet("utf8mb4")
-                    .HasCollation("utf8mb4_0900_ai_ci");
+                entity.HasOne(d => d.FkidEstatusNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.FkidEstatus)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_usuarios_EstatusDeRegistros1");
             });
 
             OnModelCreatingPartial(modelBuilder);
