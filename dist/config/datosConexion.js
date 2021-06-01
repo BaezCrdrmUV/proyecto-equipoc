3ca84e87de2936b17080bc0ff8513e6bdf422ec7
aaron-77 <aaronhl1313@gmail.com>
2021-05-31 19:43:29 -0500

    type fix
    carpeta innecesaria

--- dist/config/datosConexion.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rutaServicioBusquedaPortadas = exports.rutaServicioBusquedaCanciones = void 0;
-exports.rutaServicioBusquedaCanciones = "http://192.168.100.51:8091/artistas/buscar";
-exports.rutaServicioBusquedaPortadas = "http://192.168.100.51:8092/multimedia/buscarPortadaArtista/";
-//# sourceMappingURL=datosConexion.js.map
