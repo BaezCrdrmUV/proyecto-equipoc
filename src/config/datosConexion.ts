61f501aee9575ac2446d51b2670f453650a3a119
jonathan <jhoni65475@gmail.com>
2021-06-03 15:15:32 -0500

    Agergando login al api

--- src/config/datosConexion.ts
+++ src/config/datosConexion.ts
@@ -1,2 +1,3 @@
 export const rutaServicioBusquedaCanciones = "http://192.168.100.51:8091/artistas/buscar"
 export const rutaServicioBusquedaPortadas = "http://192.168.100.51:8092/multimedia/buscarPortadaArtista/"
+export const rutaServicioLogin = "http://192.168.100.51:8092/controller/login"
