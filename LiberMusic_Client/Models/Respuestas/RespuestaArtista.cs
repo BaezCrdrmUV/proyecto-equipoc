661eadd5e09365439e4da6ee3309b3987c75770e
jonathan <jhoni65475@gmail.com>
2021-06-11 18:05:59 -0500

    me faltó

--- LiberMusic_Client/Models/Respuestas/RespuestaArtista.cs
+++ LiberMusic_Client/Models/Respuestas/RespuestaArtista.cs
@@ -8,5 +8,9 @@ namespace LiberMusic_Client.Models.Respuestas
 {
     class RespuestaArtista
     {
+        public bool estatus { get; set; }
+        public string mensaje { get; set; }
+        public Artistas datos { get; set; }
+        public string[] errores { get; set; }
     }
 }
