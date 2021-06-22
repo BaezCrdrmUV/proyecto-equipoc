c36f7cf42b9252faa106d02b6cf0fa2ce2865fc7
jonathan <jhoni65475@gmail.com>
2021-06-22 00:23:19 -0500

    Agregando las variables de entorno

--- src/config/datosConexion.ts
+++ src/config/datosConexion.ts
@@ -1,6 +1,7 @@
 
 
-export const rutaServicioLogin = "http://localhost:8090/suscripcion/login"
+export const rutaServicioLogin = process.env.URL_MS_Suscripcion+"/suscripcion/login" 
 export const rutaServicioRegistrar = "http://localhost:8090/suscripcion/RegistrarUsuario"
 export const rutaServicioActualizarUsuario = "http://localhost:8090/suscripcion/actualizarUsuario"
 
+//"http://localhost:8090/suscripcion/login"
