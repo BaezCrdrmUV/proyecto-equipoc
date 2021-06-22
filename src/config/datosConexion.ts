1c42c7099da6fe363ce40e18a2d52bd718ec8b1f
jonathan <jhoni65475@gmail.com>
2021-06-22 00:38:19 -0500

    probando mas rutas

--- src/config/datosConexion.ts
+++ src/config/datosConexion.ts
@@ -2,6 +2,6 @@
 
 export const rutaServicioLogin = process.env.URL_MS_Suscripcion+"/suscripcion/login" 
 export const rutaServicioRegistrar = process.env.URL_MS_Suscripcion+"/suscripcion/RegistrarUsuario"
-export const rutaServicioActualizarUsuario = "http://localhost:8090/suscripcion/actualizarUsuario"
+export const rutaServicioActualizarUsuario = process.env.URL_MS_Suscripcion+"/suscripcion/actualizarUsuario"
 
 //"http://localhost:8090/suscripcion/login"
